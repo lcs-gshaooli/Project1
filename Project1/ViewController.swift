4f70dcc542dc278cc8ec6f9457426b5535fb196e
Gabriela Shaooli <gshaooli@lcs.on.ca>
2020-04-16 10:22:50 -0500

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Gabriela Shaooli on 2020-04-16.
+//  Copyright © 2020 Gabriela Shaooli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
