df7d95561a93bd3947a48aa3bdd02736c7c6dbed
Gabriela Shaooli <gshaooli@lcs.on.ca>
2020-04-16 10:55:58 -0500

    Star project 1, upload the list of images.

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    var pictures = [String]()
+    
+    // MARK: Initalizers
+    
+    
+    // MARK: Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
