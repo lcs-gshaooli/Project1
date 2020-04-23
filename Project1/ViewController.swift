b8205ed36ac56e8d1b27bf8e9830907826e42530
Gabriela Shaooli <gshaooli@lcs.on.ca>
2020-04-22 23:24:57 -0500

    Load images, and fix some problems of the storyboard

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -40,9 +40,16 @@ class ViewController: UITableViewController {
     return pictures.count
 
 }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-    cell.textLabel?.text = pictures[indexPath.row]
-        return cell 
-}
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
